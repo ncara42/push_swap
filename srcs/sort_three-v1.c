5a4c29a1a6ddfe74f3f10497fc6a54e6282f31c4
ncara42 <noel@noelcaravaca.com>
2026-01-11 04:37:28 +0100

    100% norminette

--- srcs/sort_three-v1.c
+++ srcs/sort_three-v1.c
@@ -6,19 +6,41 @@
 /*   By: admin <admin@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/02 17:58:54 by vvan-ach          #+#    #+#             */
-/*   Updated: 2026/01/11 04:11:29 by admin            ###   ########.fr       */
+/*   Updated: 2026/01/11 04:35:59 by admin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
-void	sort_three_top(t_stacks s, t_stats **stats, t_fst *fst, int bench)
+static void	update_st(t_stats **stats, int sa, int pb, int pa)
 {
-	fst->first = (*(s.stack_a))->content;
-	fst->second = (*(s.stack_a))->next->content;
-	fst->third = (*(s.stack_a))->next->next->content;
-	sort_three_top_next(s, stats, fst, bench);
-	sort_three_top_next_next(s, stats, fst, bench);
+	if (!stats || !*stats)
+		return ;
+	(*stats)->sa_count += sa;
+	(*stats)->pb_count += pb;
+	(*stats)->pa_count += pa;
+}
+
+void	sort_three_top_next_next(t_stacks s, t_stats **st, t_fst *f, int b)
+{
+	if (f->first < f->second && f->second > f->third && f->first < f->third)
+	{
+		pb(s.stack_a, s.stack_b);
+		sa(s.stack_a);
+		pa(s.stack_a, s.stack_b);
+		if (b)
+			update_st(st, 1, 1, 1);
+	}
+	else if (f->first > f->second && f->second > f->third)
+	{
+		sa(s.stack_a);
+		pb(s.stack_a, s.stack_b);
+		sa(s.stack_a);
+		pa(s.stack_a, s.stack_b);
+		sa(s.stack_a);
+		if (b)
+			update_st(st, 3, 1, 1);
+	}
 }
 
 void	sort_three_top_next(t_stacks s, t_stats **stats, t_fst *fst, int bench)
@@ -28,7 +50,7 @@ void	sort_three_top_next(t_stacks s, t_stats **stats, t_fst *fst, int bench)
 	{
 		sa(s.stack_a);
 		if (bench)
-			(*stats)->sa_count++;
+			update_st(stats, 1, 0, 0);
 	}
 	else if (fst->first > fst->second && fst->second < fst->third
 		&& fst->first > fst->third)
@@ -38,13 +60,18 @@ void	sort_three_top_next(t_stacks s, t_stats **stats, t_fst *fst, int bench)
 		sa(s.stack_a);
 		pa(s.stack_a, s.stack_b);
 		if (bench)
-		{
-			(*stats)->sa_count += 2;
-			(*stats)->pb_count++;
-			(*stats)->pa_count++;
-		}
+			update_st(stats, 2, 1, 1);
 	}
-	else if (fst->first < fst->second && fst->second > fst->third
+}
+
+void	sort_three_top(t_stacks s, t_stats **stats, t_fst *fst, int bench)
+{
+	fst->first = (*(s.stack_a))->content;
+	fst->second = (*(s.stack_a))->next->content;
+	fst->third = (*(s.stack_a))->next->next->content;
+	sort_three_top_next(s, stats, fst, bench);
+	sort_three_top_next_next(s, stats, fst, bench);
+	if (fst->first < fst->second && fst->second > fst->third
 		&& fst->first > fst->third)
 	{
 		pb(s.stack_a, s.stack_b);
@@ -52,44 +79,7 @@ void	sort_three_top_next(t_stacks s, t_stats **stats, t_fst *fst, int bench)
 		pa(s.stack_a, s.stack_b);
 		sa(s.stack_a);
 		if (bench)
-		{
-			(*stats)->pb_count++;
-			(*stats)->sa_count += 2;
-			(*stats)->pa_count++;
-		}
-	}
-}
-
-void	sort_three_top_next_next(t_stacks s, t_stats **stats, t_fst *fst,
-			int bench)
-{
-	if (fst->first < fst->second && fst->second > fst->third
-		&& fst->first < fst->third)
-	{
-		pb(s.stack_a, s.stack_b);
-		sa(s.stack_a);
-		pa(s.stack_a, s.stack_b);
-		if (bench)
-		{
-			(*stats)->pb_count++;
-			(*stats)->sa_count++;
-			(*stats)->pa_count++;
-		}
-	}
-	else if (fst->first > fst->second
-		&& fst->second > fst->third)
-	{
-		sa(s.stack_a);
-		pb(s.stack_a, s.stack_b);
-		sa(s.stack_a);
-		pa(s.stack_a, s.stack_b);
-		sa(s.stack_a);
-		if (bench)
-		{
-			(*stats)->sa_count += 3;
-			(*stats)->pb_count++;
-			(*stats)->pa_count++;
-		}
+			update_st(stats, 2, 1, 1);
 	}
 }
 
@@ -98,9 +88,8 @@ void	sort_three(t_stacks s, int pushed, t_stats **stats)
 	t_fst	fst;
 	int		bench;
 
-	if (!stats || !*stats)
-		bench = 0;
-	else
+	bench = 0;
+	if (stats && *stats)
 		bench = (*stats)->isbench;
 	if (pushed == 3 && ft_lstsize(*(s.stack_a)) == 3)
 		sort_three_a(s.stack_a, stats, bench);
@@ -112,7 +101,7 @@ void	sort_three(t_stacks s, int pushed, t_stats **stats)
 		{
 			sa(s.stack_a);
 			if (bench)
-				(*stats)->sa_count++;
+				update_st(stats, 1, 0, 0);
 		}
 	}
 }
